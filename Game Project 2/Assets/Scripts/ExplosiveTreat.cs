accc1c37475dee19f05842f2021cc05ef95a8111
Constance Wang <constancezwang@gmail.com>
2023-04-12 19:43:31 -0400

    explosive treat collision handling

--- Game Project 2/Assets/Scripts/ExplosiveTreat.cs
+++ Game Project 2/Assets/Scripts/ExplosiveTreat.cs
@@ -12,24 +12,42 @@ public class ExplosiveTreat : Treat
     [SerializeField]
     private GameObject explosionVolume;
 
-    private void HandleTreatCollision(Treat treat)
+    private void HandleTreatCollision(SweetTreat treat)
     {
-
+        Debug.Log("Explosive collided with treat");
+        Explode();
     }
 
     private void HandlePlayerCollision(PlayerManager player)
     {
-
+        Debug.Log("Explosive collided with player: taking " + Damage + " damage");
+        player.TakeDamage(Damage);
+        Explode();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-
+        Debug.Log("Collision with explosive detected");
+        Collider collider = collision.collider;
+        // TODO: add "Player" tag to player body
+        // Also add "PlayerHand" tag to player left/right hand controllers and check those
+        if (collider.CompareTag("Player")) {
+            PlayerManager player = collider.gameObject.GetComponent<PlayerManager>();
+            HandlePlayerCollision(player);
+        }
+        else if (collider.CompareTag("SweetTreat"))
+        {
+            SweetTreat treat = collider.gameObject.GetComponent<SweetTreat>();
+            HandleTreatCollision(treat);
+            Destroy(collider.gameObject);
+        }
     }
 
     public void Explode()
     {
-
+        Debug.Log("Spawning explosion");
+        Instantiate(explosionVolume, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     // Start is called before the first frame update
@@ -41,6 +59,6 @@ public class ExplosiveTreat : Treat
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Game Project 2/Assets/Scripts/Treat.cs
+++ Game Project 2/Assets/Scripts/Treat.cs
@@ -16,7 +16,7 @@ public abstract class Treat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
