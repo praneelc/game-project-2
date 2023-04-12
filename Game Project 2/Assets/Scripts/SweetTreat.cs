f5792826614506fea7cde4c74076d0fa43cf8706
hansen-yi <hansenyi@seas.upenn.edu>
2023-04-12 16:57:08 -0400

    Reset Unity Project for Vive

---
+++ Game Project 2/Assets/Scripts/SweetTreat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweetTreat : Treat
+{
+    public bool wasThrown;
+
+    private void HandleTreatCollision(Treat treat)
+    {
+
+    }
+
+    private void HandlePlayerCollision(PlayerManager player)
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
