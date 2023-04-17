89b2b19d725cad9a39e29a989c0f44d2edcdfc81
hansen-yi <hansenyi@seas.upenn.edu>
2023-04-17 17:00:17 -0400

    started treat spawning logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     private PlayerHandController leftHand;
     private PlayerHandController rightHand;
 
+    private PlayerManager player;
     private void Awake()
     {
         
@@ -27,7 +28,8 @@ public class GameManager : MonoBehaviour
     {
         leftHand = GameObject.Find("LeftController").GetComponent<PlayerHandController>();
         rightHand = GameObject.Find("RightController").GetComponent<PlayerHandController>();
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+
         inputActions["LeftHandClose"].performed += _ => leftHand.CloseHand();
         inputActions["LeftHandClose"].canceled += _ => leftHand.OpenHand();
         inputActions["RightHandClose"].performed += _ => rightHand.CloseHand();
@@ -70,14 +72,57 @@ public class GameManager : MonoBehaviour
     #endregion
 
     #region Game Play
-    public void SpawnSweetTreat()
+
+    [SerializeField]
+    float angleSpawnRange = 60;
+    [SerializeField]
+    private float spawnDist = 10f;
+
+    [Header("TreatSpawning")]
+    [SerializeField]
+    private List<GameObject> sweetTreatPrefabs;
+    [SerializeField]
+    private List<GameObject> explosiveTreatPrefabs;
+
+    public void SpawnTreat()
     {
+        // Select a random direction from appropriate range around player
+        float randAngle = Random.Range(-angleSpawnRange, angleSpawnRange);
+        Vector3 spawnDir = Quaternion.Euler(0, randAngle,0) * player.transform.forward;
+
+        // Select a point horizon-distance away along that line
+        Vector3 spawnPoint = player.transform.position + spawnDist * spawnDir;
+
+        // Determine the velocity needed from that point assuming UnityEngine.Physics.gravity to make the treat target the player
+        // get velocity direction (z and x components only) from spawn direction
+        // pick random angle (angle from ground) for launch, and calculate velocity magnitude accordingly 
+        float randVelAngle = Random.Range(30, 60);
+        float velMag = spawnDist / (Mathf.Cos(randVelAngle)) * Mathf.Sqrt(UnityEngine.Physics.gravity.y/(2*spawnDist*Mathf.Tan(randVelAngle)));
+        Vector3 spawnVelocity = velMag*Mathf.Cos(randVelAngle)*(-Vector3.Normalize(spawnDir)+Vector3.up*Mathf.Tan(randVelAngle));
+
+
+
+        // Random float; if <= explosiveChance, spawn explosive treat, else sweettreat
+        float f = Random.Range(0, 1);
+
+        if (f < this.explosiveChance)
+        {
+            SpawnExplosive(spawnPoint, spawnVelocity);
+        } else
+        {
+            SpawnSweetTreat(spawnPoint, spawnVelocity);
+        }
 
     }
-
-    public void SpawnExplosive()
+    
+    public void SpawnSweetTreat(Vector3 pos, Vector3 velocity)
     {
+        // TODO: determine spawning algorithm
+    }
 
+    public void SpawnExplosive(Vector3 pos, Vector3 velocity)
+    {
+        // TODO
     }
 
     public void SpawnTarget()
